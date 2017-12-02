13fb40841716414e261aba5853a036f42cc1eecb
Jacob Allen <jacob@polyomino.xyz>
2017-12-02 01:49:00 +0000

    Redesigned module structure and implemented a few more instructions

--- src/instructions.rs
+++ src/instructions.rs
@@ -8,7 +8,7 @@ pub struct Instruction<'a> {
   run: &'a Fn(&mut Machine),
 }
 
-pub const INSTRUCTION_COUNT: usize = 8;
+pub const INSTRUCTION_COUNT: usize = 9;
 
 pub const INSTRUCTIONS: [Instruction; INSTRUCTION_COUNT] = [
   Instruction {
@@ -16,7 +16,7 @@ pub const INSTRUCTIONS: [Instruction; INSTRUCTION_COUNT] = [
     num_args: 0,
     op_code: 0x0,
     run: &|machine: &mut Machine| {
-      machine.ip_inc();
+      machine.instruction_pointer += 1;
     }
   },
   Instruction {
@@ -24,26 +24,49 @@ pub const INSTRUCTIONS: [Instruction; INSTRUCTION_COUNT] = [
     num_args: 0,
     op_code: 0xff,
     run: &|machine: &mut Machine| {
-      machine.flag_set_halt(true);
+      machine.flags.halt = true;
+    }
+  },
+  Instruction {
+    inst: "LOAD",
+    num_args: 1,
+    op_code: 0x01,
+    run: &|machine: &mut Machine| {
+      
     }
   },
   Instruction {
     inst: "PUSH",
     num_args: 0,
     op_code: 0x10,
-    run: &|x: &mut Machine| {}
+    run: &|machine: &mut Machine| {
+      machine.stack.push(machine.accumulator);
+    }
   },
   Instruction {
     inst: "POP",
     num_args: 0,
     op_code: 0x11,
-    run: &|x: &mut Machine| {}
+    run: &|machine: &mut Machine| {
+      machine.accumulator = machine.stack.pop();
+    }
   },
   Instruction {
     inst: "ADD",
     num_args: 0,
     op_code: 0x20,
-    run: &|x: &mut Machine| {}
+    run: &|machine: &mut Machine| {
+      let temp1: u16 = machine.stack.pop();
+      let temp2: u16 = machine.stack.pop();
+      let mut carry: u16 = temp1 & temp2;
+      let mut result: u16 = temp1 ^ temp2;
+      for _ in 0..16 {
+        let shifted_carry: u16 = carry << 1;
+        carry = result & shifted_carry;
+        result ^= shifted_carry;
+      }
+      machine.accumulator = result;
+    }
   },
   Instruction {
     inst: "ADDC",
--- src/machine/mod.rs
+++ src/machine/mod.rs
@@ -1,19 +1,26 @@
 //! Representation of the Rusty Jello machine
 
-struct Flags{
-  halt: bool,
-  carry: bool,
-  overflow: bool,
-  test: bool,
+pub enum Register{
+  R0 = 0,
+  R1 = 1,
+  R2 = 2,
+  R3 = 3
 }
 
-struct Stack {
+pub struct Flags {
+  pub halt: bool,
+  pub carry: bool,
+  pub overflow: bool,
+  pub test: bool,
+}
+
+pub struct Stack {
   stack_pointer: u8,
   stack: [u16; 16],
 }
 
 impl Stack {
-  fn new() -> Stack {
+  pub fn new() -> Stack {
     return Stack {
       stack_pointer: 0,
       stack: [0; 16],
@@ -22,7 +29,7 @@ impl Stack {
 }
 
 impl Stack {
-  fn push(&mut self, item: u16) -> bool {
+  pub fn push(&mut self, item: u16) -> bool {
     if self.stack_pointer < 15 {
       self.stack_pointer += 1;
       self.stack[self.stack_pointer as usize] = item;
@@ -34,10 +41,7 @@ impl Stack {
     self.stack[self.stack_pointer as usize] = item;
     return false;
   }
-}
-
-impl Stack {
-  fn pop(&mut self) -> u16 {
+  pub fn pop(&mut self) -> u16 {
     if self.stack_pointer > 0 {
       self.stack_pointer -= 1;
       return self.stack[self.stack_pointer as usize + 1];
@@ -47,13 +51,13 @@ impl Stack {
 }
 
 pub struct Machine {
-  memory: [u8; 65536],
-  registers: [u16; 4],
-  accumulator: u16,
-  instruction_pointer: u16,
-  stack: Stack,
-  instruction_pointer_stack: Stack,
-  flags: Flags,
+  pub memory: [u8; 65536],
+  pub registers: [u16; 4],
+  pub accumulator: u16,
+  pub instruction_pointer: u16,
+  pub stack: Stack,
+  pub instruction_pointer_stack: Stack,
+  pub flags: Flags,
 }
 
 impl Machine {
@@ -65,56 +69,12 @@ impl Machine {
       instruction_pointer: 0,
       stack: Stack::new(),
       instruction_pointer_stack: Stack::new(),
-      flags: Flags{halt: false, carry: false, overflow: false, test: false}
+      flags: Flags {
+        halt: false,
+        carry: false,
+        overflow: false,
+        test: false,
+      },
     };
   }
 }
-
-impl Machine {
-  pub fn ip_inc(&mut self){
-    self.instruction_pointer += 1;
-  }
-}
-
-/// Functions for setting the machine flags 
-impl Machine {
-  pub fn flag_set_halt(&mut self, val: bool){
-    self.flags.halt = val;
-  }
-  pub fn flag_set_carry(&mut self, val: bool){
-    self.flags.carry = val;
-  }
-  pub fn flag_set_overflow(&mut self, val: bool){
-    self.flags.overflow = val;
-  }
-  pub fn flag_set_test(&mut self, val: bool){
-    self.flags.halt = val;
-  }
-}
-
-/// Functions for getting the machine flags
-impl Machine {
-  pub fn flag_get_halt(&self) -> bool{
-    return self.flags.halt;
-  }
-  pub fn flag_get_carry(&self) -> bool{
-    return self.flags.carry;
-  }
-  pub fn flag_get_overflow(&self) -> bool{
-    return self.flags.overflow;
-  }
-  pub fn flag_get_test(&self) -> bool{
-    return self.flags.halt;
-  }
-}
-
-/// Functions for interacting with machine memory
-impl Machine{
-  pub fn mem_read(&self, location: u16) -> u8{
-    return self.memory[location as usize];
-  }
-  pub fn mem_write(&mut self, location: u16, data: u8){
-    self.memory[location as usize] = data;
-  }
-}
-
